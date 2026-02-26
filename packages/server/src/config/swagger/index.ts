616b20ee0d6c8ca94d0b83a183ea3d7f82b72e06
ihebennachet <iheb.nachet@digidco.com>
2026-02-26 13:29:33 +0100

    Update index.ts

--- packages/server/src/config/swagger/index.ts
+++ packages/server/src/config/swagger/index.ts
@@ -2,32 +2,27 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
-  const config = new DocumentBuilder()
-    .setTitle('Finora API')
-    .setDescription('API documentation for Finora')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        name: 'JWT',
-        description: 'Enter JWT token',
-        in: 'header',
-      },
-      'JWT-auth'
-    )
-    .build();
+const config = new DocumentBuilder()
+  .setTitle('Finora API')
+  .setDescription('API documentation for Finora')
+  .setVersion('1.0')
+  .addBearerAuth(
+    {
+      type: 'http',
+      scheme: 'bearer',
+      bearerFormat: 'JWT',
+      name: 'JWT',
+      description: 'Enter JWT token',
+      in: 'header',
+    },
+    'JWT-auth'
+  )
+  .addServer('/')
+  .build();
 
   const document = SwaggerModule.createDocument(app, config);
 
-  // Force HTTP URLs in Swagger
-  document.servers = [
-    {
-      url: `http://192.168.1.185:${process.env.PORT || 3000}`,
-      description: 'Local HTTP server',
-    },
-  ];
+
 
   SwaggerModule.setup('docs', app, document);
 

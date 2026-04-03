907af8ecad265c78573cb206c5907b2b386afadd
Russ-bensalah <bensalah.raslen@digidco.com>
2026-04-03 17:08:43 +0100

    fix(raslen): fix message button

--- packages/client/dev-dist/sw.js
+++ packages/client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e2640689'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.epkco4g3j4c"
+    "revision": "0.vhdvi6ithc4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
